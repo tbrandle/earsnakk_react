a9c300b8bb4e45aab0e5debe84370cbf728e903d
Tim <tbrandle53@gmail.com>
2017-06-03 10:45:35 -0600

    Login button connected

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './Login.css'
+
+const spotifyAuth = () => {
+  fetch('/auth/spotify')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log(error))
+}
 
 const Login = () => {
   return (
     <div>
       <h1>welcom to earsnakk</h1>
       <p>click here to login with Spotify and start sharing music with your friends</p>
-      <button>Login</button>
+      <a className="login-link" href="http://localhost:8888/auth/spotify">Login</a>
     </div>
   )
 }

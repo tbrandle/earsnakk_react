0545cdbf31089c4fd824cb89f79ca32885383b0e
Julian Kyer <julian.kyer@gmail.com>
2017-06-03 16:04:43 -0600

    Add basic styles to find-channel

--- src/components/FindChannel/FindChannel.js
+++ src/components/FindChannel/FindChannel.js
@@ -4,11 +4,27 @@ import './FindChannel.css';
 
 const FindChannel = () => {
   return (
-    <div>
+    <div className="find-channel-wrapper">
       <h3>Available Channels</h3>
-      <Link to="/franklin-channel"><p>Franklin Loves Daft Punk</p></Link>
-      <p>DJ Llama</p>
-      <p>Born to Rage</p>
+      
+      <Link to="/franklin-channel">
+        <div className="find-channel-link">
+          <p>Franklin Loves Daft Punk</p>
+        </div>
+      </Link>
+      
+      <Link to="/franklin-channel">
+        <div className="find-channel-link">
+          <p>Franklin Loves Daft Punk</p>
+        </div>
+      </Link>
+      
+      <Link to="/franklin-channel">
+        <div className="find-channel-link">
+          <p>Franklin Loves Daft Punk</p>
+        </div>
+      </Link>
+      
     </div>
   );
 }

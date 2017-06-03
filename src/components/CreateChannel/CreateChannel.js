a29bca08cdad1bdb7cc78e91343a2611b0ef2580
Tim <tbrandle53@gmail.com>
2017-06-03 14:25:26 -0600

    CreateChannel is class component...Create bind to input and this.state

--- src/components/CreateChannel/CreateChannel.js
+++ src/components/CreateChannel/CreateChannel.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './CreateChannel.css'
 
-const handleSubmit = (e) => {
-  e.preventDefault()
-}
 
-const CreateChannel = () => {
-  return (
-    <div className="create-channel-wrapper">
-      <input className="create-channel-input" type="text" placeholder="Channel Name" />
-      <form className="create-channel-form">
-        <fieldset>
-          <legend>Choose Channel Genres</legend>
+class CreateChannel extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      genres: {}
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  render(){
+    return (
+      <div className="create-channel-wrapper">
+        <input
+          className="create-channel-input"
+          type="text"
+          placeholder="Channel Name"
+          value={this.state.name}
+          onChange={(e) =>  this.setState({ name: e.target.value }) }
+          />
+        <form className="create-channel-form">
+          <fieldset>
+            <legend>Choose Channel Genres</legend>
             <input type="checkbox" name="genres" value="Hiphop" />Hiphop <br />
             <input type="checkbox" name="genres" value="EDM" />EDM<br />
             <input type="checkbox" name="genres" value="Rock" />Rock<br />
             <input type="checkbox" name="genres" value="All Genres" />All Genres<br />
-        </fieldset>
-      </form>
-      <input onClick={(e) => handleSubmit(e) } type="submit" value="Submit now" />
-    </div>
-  )
+          </fieldset>
+        </form>
+        <input onClick={(e) => this.handleSubmit(e) } type="submit" value="Submit now" />
+      </div>
+    )
+  }
 }
+
 
 export default CreateChannel;

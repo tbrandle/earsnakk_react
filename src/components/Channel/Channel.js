d8deff980c0f1c84a4b45ba749abeed97874d135
Tim <tbrandle53@gmail.com>
2017-06-03 20:06:08 -0600

    Will render all songs on the page

--- src/components/Channel/Channel.js
+++ src/components/Channel/Channel.js
@@ -7,24 +7,36 @@ class Channel extends Component {
     super()
     this.state = {
       artist: '',
-      track: ''
+      track: '',
+      searchTracks: []
     }
+  }
+
+  // <p>artist name: ${track.name}</p>
+  displayTracks(){
+    return this.state.searchTracks.map(track => {
+      return (<div>
+        <p>track name: {track.name}</p>
+      </div>)
+    })
   }
 
   searchTracks(){
     const { artist, track } = this.state
     fetch(`/api/v1/${artist}/search-tracks`)
       .then(response => response.json())
-      .then(songs => console.log(songs))
+      .then(songs => this.setState({searchTracks: songs.tracks.items}))
       .catch(error => console.log(error))
   }
 
   render(){
+    console.log(this.state.searchTracks);
     return (
       <div>
         <input type="text" placeholder="artist" onChange={(e) => this.setState({ artist: e.target.value })} value={this.state.artist}/>
         <input type="text" placeholder="song" onChange={(e) => this.setState({ track: e.target.value })} value={this.state.track}/>
         <button onClick={() => this.searchTracks()}>submit</button>
+        { this.state.searchTracks.length && this.displayTracks() }
         <h1>PLAYER WIDGET</h1>
         <ul>
           <li>Tim</li>

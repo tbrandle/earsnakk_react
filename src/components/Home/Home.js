7c7317415a5641debf3ed8b429e9d9079b0062d7
Franklin Crosby <obleo33@gmail.com>
2017-06-05 11:26:57 -0600

    Set up get endpoint for playlists

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,15 +5,35 @@ import './Home.css';
 class Home extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      channels: [],
+    }
   }
 
-  componentDidMount(){
+  componentWillMount(){
     this.props.profileFetch()
+  }
+
+  componentDidMount(page) {
+    let offset = page || 0
+    this.myPlaylists(offset)
   }
 
   // newChannel(){
   //   fetch()
   // }
+
+  myPlaylists(offset) {
+    fetch(`/api/v1/user/playlists/${offset}`, {
+    })
+      .then(response => response.json())
+      .then(data => this.earsnakkChannels(data))
+  }
+
+  earsnakkChannels(data) {
+    console.log(data)
+  }
+
 
   render(){
     return (

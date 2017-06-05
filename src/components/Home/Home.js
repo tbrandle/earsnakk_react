85d27d80e24d712846b81a1c7553a26df1900743
Franklin Crosby <obleo33@gmail.com>
2017-06-05 13:21:36 -0600

    Add previous playlist load and click events

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import './Home.css';
+import Routes from '../Routes/Routes';
 
 class Home extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      channels: [],
+      earsnakkPlaylists: [],
     }
   }
 
@@ -15,23 +16,48 @@ class Home extends Component {
   }
 
   componentDidMount(page) {
-    let offset = page || 0
-    this.myPlaylists(offset)
+    this.myPlaylists(0)
   }
-
-  // newChannel(){
-  //   fetch()
-  // }
 
   myPlaylists(offset) {
     fetch(`/api/v1/user/playlists/${offset}`, {
     })
       .then(response => response.json())
-      .then(data => this.earsnakkChannels(data))
+      .then(data => this.earsnakkChannels(data, offset))
   }
 
-  earsnakkChannels(data) {
-    console.log(data)
+  earsnakkChannels(data, offset) {
+    let playlistArr = data.items
+
+    playlistArr.map(playlist => {
+      if (playlist.name.includes('earsnakk_')){
+        this.setState({ earsnakkPlaylists: [...this.state.earsnakkPlaylists, playlist] })
+      }
+    })
+
+    if (data.next !== null || !playlistArr.length){
+      this.myPlaylists(offset+50)
+    }
+
+  }
+
+  handleClick(playlist){
+    this.props.loadEarsnakkPlaylist(playlist)
+  }
+
+  routeToPlaylist(id) {
+    return <Redirect to={`/channel/${id}`}/>
+  }
+
+  yourPlaylists(){
+    if(this.state.earsnakkPlaylists.length){
+      return this.state.earsnakkPlaylists.map(playlist => {
+        return (
+          <div id={playlist.id} onClick={ () => this.handleClick(playlist) }>{playlist.name}</div>
+        )
+      })
+
+    }
   }
 
 
@@ -40,6 +66,10 @@ class Home extends Component {
       <div className="home-wrapper">
         <Link to="/create-channel" className="button-link">New Channel</Link>
         <Link to="/find-channel" className="button-link">Find A Channel</Link>
+        <div className="earsnakk-playlists">
+          {this.yourPlaylists()}
+        </div>
+        {this.props.playlist.id && this.routeToPlaylist(this.props.playlist.id)}
       </div>
     )
   }

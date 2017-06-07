202e97fea912a6aa3c2ef706b45a740890a49f73
Tim <tbrandle53@gmail.com>
2017-06-06 22:03:21 -0600

    Change fold path in server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use(session({ secret: 'keyboard cat' }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/react-ui/build'));
 
 app.engine('html', consolidate.swig);
 
